77523030a40168fa79b24aa086c51ba885b286f0
Juri Kern <jkern@malwarebytes.com>
2024-07-05 03:27:17 -0700

    init

---
+++ lua/init.lua
@@ -0,0 +1,28 @@
+vim.opt.number = true
+vim.wo.relativenumber = true
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argv(0) == "" then
+      require("telescope.builtin").find_files()
+    end
+  end,
+})
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.current_buffer_fuzzy_find, {})
+
+vim.api.nvim_set_option("clipboard","unnamedplus")
+
+
+
+
+
+
+
+
+
+
+
+
+
