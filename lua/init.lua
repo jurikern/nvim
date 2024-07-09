8c8d9d545be8626808d90915d673f61374f98030
Juri Kern <jkern@malwarebytes.com>
2024-07-09 09:24:49 -0700

    remove spaces

--- lua/init.lua
+++ lua/init.lua
@@ -14,15 +14,11 @@ vim.keymap.set('n', '<leader>fb', builtin.current_buffer_fuzzy_find, {})
 
 vim.api.nvim_set_option("clipboard","unnamedplus")
 
-
-
-
-
-
-
-
-
-
-
-
-
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    pattern = {"*"},
+    callback = function()
+      local save_cursor = vim.fn.getpos(".")
+      pcall(function() vim.cmd [[%s/\s\+$//e]] end)
+      vim.fn.setpos(".", save_cursor)
+    end,
+})
