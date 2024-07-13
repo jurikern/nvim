c4f5f2cbcb021b4df941f711a029ee89b221c289
Juri Kern <jkern@malwarebytes.com>
2024-07-13 05:25:07 -0700

    sync changes

--- lua/home/packer.lua
+++ lua/home/packer.lua
@@ -14,16 +14,20 @@ return require('packer').startup(function(use)
   use { "ThePrimeagen/harpoon" }
   use { "mbbill/undotree" }
   use { "tpope/vim-fugitive" }
---  use {
---    "VonHeikemen/lsp-zero.nvim",
---    branch = "v3.x",
---    requires = {
---      {'williamboman/mason.nvim'},
---      {'williamboman/mason-lspconfig.nvim'},
---      {"neovim/nvim-lspconfig"},
---      {"hrsh7th/nvim-cmp"},
---      {"hrsh7th/cmp-nvim-lsp"},
---      {"L3MON4D3/LuaSnip"},
---    }
---  }
+  use {
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v3.x",
+    requires = {
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+      {"neovim/nvim-lspconfig"},
+      {"hrsh7th/nvim-cmp"},
+      {"hrsh7th/cmp-nvim-lsp"},
+      {"L3MON4D3/LuaSnip"},
+    }
+  }
+  use 'ray-x/go.nvim'
+  use 'ray-x/guihua.lua'
+  use 'neovim/nvim-lspconfig'
+  use 'nvim-treesitter/nvim-treesitter'
 end)
